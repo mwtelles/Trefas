3aa780e871cf48c36d549323b07c3008e6069a2f
Matheus Telles <business.mwtelles@gmail.com>
2022-10-26 12:28:07 -0300

    adicionando as tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,32 @@ import { useState } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/Item';
 import { ListItem } from './components/ListItem';
+import { InputArea } from './components/InputArea';
 
 const App = () => {
 
   const [list, setList] = useState<Item[]>([
-    { id: 1, name: 'Comprar pão', done: false },
-    { id: 2, name: 'Comprar leite', done: true },
   ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
+  
 
   return (
     <C.Container> 
       <C.Area>
         <C.Header>Tarefas</C.Header>
+
+        <InputArea onEnter={handleAddTask} />
+
         {list.map((item, index) => (
         <ListItem key={index} item={item} />
       ))}
---
+++ src/components/InputArea/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    border: 1px solid #20212C;
+    border-radius: 15px;
+    padding: 10px;
+    margin: 20px 0;
+    display: flex;
+    align-items: center;
+
+    .image {
+        margin-right: 5px;
+    }
+
+    input {
+        border: none;
+        outline: none;
+        background: transparent;
+        color: #CCC;
+        font-size: 18px;
+        flex: 1;
+    }
+`;
