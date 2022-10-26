855963bfd7b1624db79955560e5113ea54f5d3a6
Matheus Telles <business.mwtelles@gmail.com>
2022-10-26 12:33:04 -0300

    finalizando o projeto alterando o estado do input

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -4,14 +4,14 @@ import { useState } from "react";
 
 type Props = {
   item: Item;
+  onChange: (id: number, done: boolean) => void;
 };
 
-export const ListItem = ({ item }: Props) => {
-  const [isChecked, setIsChecked] = useState(item.done);
+export const ListItem = ({ item, onChange }: Props) => {
 
   return (
-    <C.Container done={isChecked}>
-      <input type="checkbox" defaultChecked={isChecked} onChange={e => setIsChecked(e.target.checked)}/>
+    <C.Container done={item.done}>
+      <input type="checkbox" onChange={e => onChange(item.id, e.target.checked)}/>
       <label>{item.name}</label>
     </C.Container>
   );
